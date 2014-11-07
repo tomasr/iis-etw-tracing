72e953fc599ee6954fbd06f6e8a7e929e8eea9f7
Tomas Restrepo <tomas@winterdom.com>
2014-11-06 21:01:32 -0500

    Adding BasicPartitionKeyGenerator and tests

---
+++ Collector/TraceProcessor.Tests/BasicPartitionKeyGeneratorTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Diagnostics.Tracing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Winterdom.Diagnostics.TraceProcessor;
+using Winterdom.Diagnostics.TraceProcessor.Impl;
+using Winterdom.Diagnostics.Tracing.IISTraceEvent;
+
+namespace TraceProcessor.Tests {
+  [TestClass]
+  [DeploymentItem("iis.etl")]
+  public class BasicPartitionKeyGeneratorTests {
+
+    [TestMethod]
+    public void PartitionKeyIncludesTraceProvider() {
+      var traceEvent = LoadSampleTrace("iis.etl").First();
+      IPartitionKeyGenerator gen = new BasicPartitionKeyGenerator();
+      String key = gen.GetKey(traceEvent);
+      Assert.IsTrue(key.IndexOf(traceEvent.ProviderGuid.ToString()) >= 0);
+    }
+
+    [TestMethod]
+    public void PartitionKeyIncludesMachineName() {
+      var traceEvent = LoadSampleTrace("iis.etl").First();
+      IPartitionKeyGenerator gen = new BasicPartitionKeyGenerator();
+      String key = gen.GetKey(traceEvent);
+      Assert.IsTrue(key.IndexOf(Environment.MachineName) >= 0);
+    }
+
+    private IEnumerable<TraceEvent> LoadSampleTrace(string file) {
+      var traceSource = new ETWTraceEventSource(file);
+      var parser = new IISLogTraceEventParser(traceSource);
+      List<TraceEvent> events = new List<TraceEvent>();
+      parser.All += e => {
+        events.Add(e.Clone());
+      };
+      traceSource.Process();
+      return events;
+    }
+  }
+}
---
+++ Collector/TraceProcessor/Impl/BasicPartitionKeyGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Diagnostics.Tracing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Winterdom.Diagnostics.TraceProcessor.Impl {
+  /// <summary>
+  /// A basic IPartitionKeyGenerator that combines the
+  /// ETW provider with the machine where the trace
+  /// was generated
+  /// </summary>
+  public class BasicPartitionKeyGenerator : IPartitionKeyGenerator {
+    public String GetKey(TraceEvent traceEvent) {
+      return String.Format("{0}-{1}", traceEvent.ProviderGuid, Environment.MachineName);
+    }
+  }
+}
