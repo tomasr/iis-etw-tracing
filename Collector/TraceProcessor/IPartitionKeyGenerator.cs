fd9047b52c9936a7fec7a17ed842fcfa5f8386b9
Tomas Restrepo <tomas@winterdom.com>
2014-11-06 20:27:10 -0500

    Fix all nuget package references and add IPartitionKeyGenerator interface

---
+++ Collector/TraceProcessor/IPartitionKeyGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Diagnostics.Tracing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Winterdom.Diagnostics.Tracing.IISTraceProcessor {
+  public interface IPartitionKeyGenerator {
+    String GetKey(TraceEvent traceEvent);
+  }
+}
