1f2944fb762ee7eb4b3d7b4c774802ef981183ac
Tomas Restrepo <tomas@winterdom.com>
2015-07-22 21:25:48 -0500

    Simplify strategy for assigning partition keys

--- Collector/TraceProcessor/Impl/BasicPartitionKeyGenerator.cs
+++ Collector/TraceProcessor/Impl/BasicPartitionKeyGenerator.cs
@@ -8,14 +8,15 @@ using System.Threading.Tasks;
 
 namespace Winterdom.Diagnostics.TraceProcessor.Impl {
   /// <summary>
-  /// A basic IPartitionKeyGenerator that combines the
-  /// ETW provider with the machine where the trace
-  /// was generated
+  /// A basic IPartitionKeyGenerator that just uses the machine name
   /// </summary>
   [Export(typeof(IPartitionKeyGenerator))]
   public class BasicPartitionKeyGenerator : IPartitionKeyGenerator {
     public String GetKey(TraceEvent traceEvent) {
-      return String.Format("{0}-{1}", traceEvent.ProviderGuid, Environment.MachineName);
+      // Original code combined the Provider GUID with the machine name
+      // but you cannot mix events with different partition keys in the same
+      // batch.
+      return Environment.MachineName;
     }
   }
 }
